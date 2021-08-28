a817a6c496d929757d8345a6cfc39ef50bd25c82
Viktor-Asenov <79524317+Viktor-Asenov@users.noreply.github.com>
2021-08-28 17:38:35 +0300

    Solutions of the Entity Relations problems

---
+++ 02. Entity Framework Core/04. Entity Relations/P01_StudentSystem/Data/Models/Resource.cs
@@ -0,0 +1,28 @@
+﻿using P01_StudentSystem.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_StudentSystem.Data.Models
+{
+    public class Resource
+    {
+        public int ResourceId { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(2048)")]
+        public string Url { get; set; }
+
+        public ResourceType ResourceType { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
---
+++ 02. Entity Framework Core/04. Entity Relations/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using P01_StudentSystem.Data;
+using System;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //var studentSystemContext = new StudentSystemContext();
+            //studentSystemContext.Database.EnsureDeleted();
+            //studentSystemContext.Database.EnsureCreated();
+        }
+    }
+}
