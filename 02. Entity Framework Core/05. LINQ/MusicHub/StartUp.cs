3927ac715e6da7dedf0b24873ce54a33e379d642
Viktor-Asenov <79524317+Viktor-Asenov@users.noreply.github.com>
2021-08-31 18:15:01 +0300

    Solutions of the LINQ problems

---
+++ 02. Entity Framework Core/05. LINQ/MusicHub/StartUp.cs
@@ -0,0 +1,107 @@
+﻿using MusicHub.Data;
+using MusicHub.Initializer;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace MusicHub
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new MusicHubDbContext();
+            
+            //DbInitializer.ResetDatabase(db);
+
+            //Console.WriteLine(ExportAlbumsInfo(db, 9));
+            Console.WriteLine(ExportSongsAboveDuration(db, 4));
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var producerAlbums = context.Albums
+                .Where(a => a.ProducerId == producerId)
+                .Select(a => new
+                {
+                    AlbumName = a.Name,
+                    ReleaseDate = a.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                    ProducerName = a.Producer.Name,
+                    Songs = a.Songs.Select(s => new
+                    {
+                        SongName = s.Name,
+                        Price = s.Price,
+                        SongWriter = s.Writer.Name
+                    })
+                    .OrderByDescending(s => s.SongName)
+                    .ThenBy(s => s.SongWriter)
+                    .ToList(),
+                    AlbumPrice = a.Price
+                })                
+                .ToList();
+
+            foreach (var album in producerAlbums.OrderByDescending(a => a.AlbumPrice))
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}");
+                sb.AppendLine($"-ReleaseDate: {album.ReleaseDate}");
+                sb.AppendLine($"-ProducerName: {album.ProducerName}");
+                sb.AppendLine($"-Songs:");
+
+                int counter = 0;
+                foreach (var song in album.Songs)
+                {                    
+                    counter++;
+                    sb.AppendLine($"---#{counter}");
+                    sb.AppendLine($"---SongName: {song.SongName}");
+                    sb.AppendLine($"---Price: {song.Price:f2}");
+                    sb.AppendLine($"---Writer: {song.SongWriter}");
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.AlbumPrice:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var songs = context.Songs
+                .ToList()
+                .Where(s => s.Duration.TotalSeconds > duration)
+                .Select(s => new
+                {
+                    Name = s.Name,
+                    WriterName = s.Writer.Name,
+                    PerformerFullName = s.SongPerformers.Select(sp => new
+                    {
+                        FullName = sp.Performer.FirstName + " " + sp.Performer.LastName
+                    })
+                    .FirstOrDefault(),                    
+                    AlbumProducer = s.Album.Producer.Name,
+                    Duration = s.Duration
+                })
+                .OrderBy(s => s.Name)
+                .ThenBy(s => s.WriterName)
+                .ThenBy(s => s.PerformerFullName)
+                .ToList();
+
+            int counter = 0;
+            foreach (var song in songs)
+            {
+                sb.AppendLine($"-Song #{counter++}");
+                sb.AppendLine($"---SongName: {song.Name}");
+                sb.AppendLine($"---Writer: {song.WriterName}");
+                sb.AppendLine($"---Performer: {song.PerformerFullName}");
+                sb.AppendLine($"---AlbumProducer: {song.AlbumProducer}");
+                sb.AppendLine($"---Duration: {song.Duration:c}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
