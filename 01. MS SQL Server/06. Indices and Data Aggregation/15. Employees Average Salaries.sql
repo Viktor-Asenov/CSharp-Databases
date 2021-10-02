72442115b1635c4816d4fa9618c61f111d2ed863
Viktor-Asenov <79524317+Viktor-Asenov@users.noreply.github.com>
2021-10-02 15:25:13 +0300

    Update 15. Employees Average Salaries.sql

--- 01. MS SQL Server/06. Indices and Data Aggregation/15. Employees Average Salaries.sql
+++ 01. MS SQL Server/06. Indices and Data Aggregation/15. Employees Average Salaries.sql
@@ -1,3 +1,14 @@
-SELECT * INTO TempTable, 
+SELECT * INTO TempTable
 	FROM Employees
 	WHERE Salary > 30000
+
+DELETE FROM TempTable
+	WHERE ManagerID = 42
+
+UPDATE TempTable
+	SET Salary += 5000
+	WHERE DepartmentID = 1
+
+SELECT DepartmentID, AVG(Salary)
+	FROM TempTable
+	GROUP BY DepartmentID
